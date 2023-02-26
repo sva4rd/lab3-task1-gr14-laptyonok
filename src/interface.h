fbae314aaf4319f5da79decac79d3fe5ef7d527b
Yegor Laptyonok <95144497+sva4rd@users.noreply.github.com>
2023-02-26 13:40:59 +0000

    create files and add comments

---
+++ src/interface.h
@@ -0,0 +1,14 @@
+//
+//  created by sva4rd
+//
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+// calc_letter function calculates and return the quantities of some
+// letter in 3 sentences
+// implementation is in server.c file
+
+int calc_letter(const char letter, char *str1, char *str2, char *str3);
+
+#endif
---
+++ src/server.c
@@ -0,0 +1,12 @@
+//
+//  created by sva4rd
+//
+
+//  implementation of interface.h functions
+
+#include "interface.h"
+
+// letter_in_a_sentence function calculates and return the quantities of some
+// letter in a sentence
+
+int letter_in_a_sentence(const char letter, char *str);
